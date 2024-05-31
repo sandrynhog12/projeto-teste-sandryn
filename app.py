8a92cdd049b077dc1d847467708d89850dcde096
Alessandro De Jesus <sandrynhog12@gmail.com>
2024-05-31 20:58:58 -0300

    Versao 1 Atualizada

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 nome = input('Digite seu nome: ')
 idade = int(input('Digite sua idade: '))
-possui_cnh = False
+possui_cnh = True
 if idade <= 17:
     print('Vc é menor de idade! ')
 elif idade >= 18 and possui_cnh == False :
-    print('Vc apneas é de maior, porém, não possiu CNH! ')     
+    print('Vc apneas é de maior, porém, não possiu CNH! ') 
+elif idade >= 18 and possui_cnh == True :
+    print('Vc é de maior, e possiu CNH! ')        
 else:
-    print('Vc pode dirigir')    
+    print('Vc não pode Dirigir ')    
