006380f132ef3ceab13d5f9bf224f66e26f0b87d
Alessandro De Jesus <sandrynhog12@gmail.com>
2024-05-31 20:52:11 -0300

    Versão 1 Atualizada
    Possiu ou não CNH?

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Olá, Family! ')
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+possui_cnh = False
+if idade <= 17:
+    print('Vc é menor de idade! ')
+elif idade >= 18 and possui_cnh == False :
+    print('Vc apneas é de maior, porém, não possiu CNH! ')     
+else:
+    print('Vc pode dirigir')    
